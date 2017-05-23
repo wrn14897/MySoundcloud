5505b789f71e47e63dcd8a99199590865ce5b2dd
Warren Jonhow Lee <warrenjo@stanford.edu>
2017-05-23 21:21:50 +0800

    Add middlewares file

---
+++ client/src/middlewares.js
@@ -0,0 +1,18 @@
+/**********************************************************************/
+//TODO -> Let's implement thunk, logger middleware here
+/**********************************************************************/
+export const createLogger = ({getState}) => (next) => (action) => {
+    const console = window.console;
+    const prevState = getState();
+    const returnValue = next(action);
+    const nextState = getState();
+    const actionType = String(action.type);
+    const message = `action ${actionType}`;
+    console.log(`%c prev state`, `color: #9E9E9E`, prevState);
+    console.log(`%c action`, `color: #03A9F4`, action);
+    console.log(`%c next state`, `color: #4CAF50`, nextState);
+    return returnValue;
+};
+export const thunk = ({getState, dispatch}) => (next) => (action) => {
+    return next(action);
+};
